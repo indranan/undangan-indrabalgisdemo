0d0ff6a6c6b591e098ffa90f9e6b699d8a35d4e4
Nanda <39219734+indranan@users.noreply.github.com>
2026-07-10 00:54:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const fs = require('fs');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
@@ -393,6 +393,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server berjalan di port ${PORT}`);
 });
