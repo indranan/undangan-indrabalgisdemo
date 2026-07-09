6a540f5d3fbd308c6e43098e97224eafab78cee2
Nanda <39219734+indranan@users.noreply.github.com>
2026-07-10 00:48:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server berjalan di port ${PORT}`);
+});
 
 app.use(cors());
 app.use(express.json());
