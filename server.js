5b0d32376c9270664f568c2c47502234b8a3429f
Nanda <39219734+indranan@users.noreply.github.com>
2026-07-10 01:49:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -389,7 +389,7 @@ app.delete('/api/cms/music/:id', authenticateToken, (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
