0abf64b81ab05248ff57246faf1b8cbceadbb530
Nanda <39219734+indranan@users.noreply.github.com>
2026-07-10 00:51:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server berjalan di port ${PORT}`);
-});
 
 app.use(cors());
 app.use(express.json());
@@ -396,4 +393,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`Server Backend API menyala di port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server berjalan di port ${PORT}`);
+});
